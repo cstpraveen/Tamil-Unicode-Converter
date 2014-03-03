d4d9714c0746ad0d31e683a94a76e0952363e8c1
cstpraveen <praveen9482@gmail.com>
2014-03-03 13:03:21 +0530

    Putting Radio Buttons into the Editor window

--- try.py
+++ try.py
@@ -16,10 +16,13 @@ class Base:
 
        	self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_position(gtk.WIN_POS_CENTER)
+        self.window.set_border_width(5)
         self.window.set_size_request(800,600)
         self.window.connect("destroy", self.destroy)
         self.window.set_title("Tamil Unicode Converter")
 
+
+        #Create an Text Area
         TextBox.set_wrap_mode(gtk.WRAP_WORD)
         TextBox.set_editable(True)
         TextBox.set_cursor_visible(True)	
@@ -28,6 +31,7 @@ class Base:
         TextBox.set_border_window_size(gtk.TEXT_WINDOW_TOP,2)
         TextBox.set_border_window_size(gtk.TEXT_WINDOW_BOTTOM,2)
 
+        #Create an 2nd Text Area
         TextBox2.set_wrap_mode(gtk.WRAP_WORD)
         TextBox2.set_editable(True)
         TextBox2.set_cursor_visible(True)	
@@ -36,13 +40,22 @@ class Base:
         TextBox2.set_border_window_size(gtk.TEXT_WINDOW_TOP,2)
         TextBox2.set_border_window_size(gtk.TEXT_WINDOW_BOTTOM,2)
 
+        rbutton1 = gtk.RadioButton(None, "TSCII")
+        rbutton2 = gtk.RadioButton(None, "TAB")
+        rbutton3 = gtk.RadioButton(None, "TAM")
+        rbutton4 = gtk.RadioButton(None, "Bamini")
+
         self.box1=gtk.HBox()
         self.box1.pack_start(TextBox)
-
+       
         self.box2=gtk.VBox()
+        self.box2.pack_start(rbutton1)
+        self.box2.pack_start(rbutton2)
+        self.box2.pack_start(rbutton3)
+        self.box2.pack_start(rbutton4)
+
         self.box2.pack_start(self.box1)
         self.box2.pack_start(TextBox2)
-
 
         self.window.add(self.box2)
         self.window.show_all()
